d31ab12d6dbb0f1d4db8188411720913bdc65cd7
drew-sims <simsam2022@gmail.com>
2026-04-26 13:00:24 -0500

    Fixed minor bug in Problem 1 and decreased the period at which we are modeling over

--- source/main.m
+++ source/main.m
@@ -1,13 +1,11 @@
-%% Parameters
+%% Problem 1
+
 tau = 1.0;      % ns
 TL = 12.5;     % ns
 Ts = 0.2;      % ns
-t_end = 100;   % ns
+t_end = 50;   % ns
 sample_period = Ts/1000;
 
-% TODO: Weird behaviour that I cannot explain, had to subtract one sample
-% period otherwise it will double count, which makes sense in terms of the
-% periodicity of the FFT, though get_excitement are not acting as I expect.
 t = 0 : sample_period : (t_end - sample_period);
 
 %% Part A: Time-Domain Signals
@@ -45,9 +43,34 @@ subplot(2,1,2);
 semilogx(freqs, angle(H_theory), 'k', freqs, angle(H_num), 'r--');
 title('Fluorescence Phase Response'); ylabel('Phase (rad)'); xlabel('Frequency (GHz)'); grid on;
 
+%% Part C: Parameter Sweep
+
+% Sweep over sigma and t0 values
+sigma_values = [0.1, 1.0, 10]; % Example values for sigma
+t0_values = [2, 4, 6]; % Example values for t0
+results = zeros(length(sigma_values), length(t0_values));
+
+figure(3);
+
+for i = 1:length(sigma_values)
+    for j = 1:length(t0_values)
+        hd = get_detector_irf(t, t0_values(j), sigma_values(i));
+        f = ifft(fft(x) .* fft(hf));
+        d = ifft(fft(f) .* fft(hd));
+
+        results(i, j) = max(abs(d)); % Store the maximum output for each parameter combination
+        subplot(length(sigma_values), length(t0_values), (i-1)*length(t0_values) + j);
+        plot(t, d); % Plot the detector output for current parameters
+        title(['t0 = ', num2str(t0_values(j)), ', \sigma = ', num2str(sigma_values(i))]);
+        ylabel('Amp'); grid on;
+    end
+end
+
+
+
 %% Problem 3: Phasor Analysis
 
-i_info = load("FLIMhistogram.mat");
+i_info = load("../input/FLIMhistogram.mat");
 
 t_limit = (0:97) * (TL/98);   % 1x98, matches 3rd dim of H
 H = i_info.FLIMhistogram;
